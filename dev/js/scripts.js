ade395d61542f5bdb790e7468c33a26f5447ce69
NicoleNowakowski <81652088+NicoleNowakowski@users.noreply.github.com>
2021-06-10 15:13:47 -0400

    Beautifying Everything

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,13 +3,12 @@ import $ from "jquery";
 let $nav = $('nav[data-nav]');
 let isVisible = false;
 
-$(".nav-btns").on("click", function(){
+$(".nav-btns").on("click", function () {
     console.log("click");
-    if(isVisible === false){
+    if (isVisible === false) {
         $nav.show();
         isVisible = true;
-    }
-    else{
+    } else {
         $nav.hide();
         isVisible = false;
     }
